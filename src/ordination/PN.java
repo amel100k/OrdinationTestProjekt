cc7cf9bab4d32dab341cdaf2dd9802b5631a4985
amel100k <eaa23ema@students.eaaa.dk>
2024-03-11 10:41:20 +0100

    hh

--- src/ordination/PN.java
+++ src/ordination/PN.java
@@ -9,8 +9,9 @@ public class PN extends Ordination{
     private double antalEnheder;
     private List datoer = new ArrayList();
 
-    public PN(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel) {
+    public PN(LocalDate startDen, LocalDate slutDen, Patient patient, Laegemiddel laegemiddel, double antalEnheder) {
         super(startDen, slutDen, laegemiddel);
+        this.antalEnheder = antalEnheder;
     }
 
     /**
