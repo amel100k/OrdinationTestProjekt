c556d9f141448f30721e49b96c593f761ee7953f
Simon Høgh <simonahlehoegh@gmail.com>
2024-03-20 09:18:08 +0100

    test exeption

--- test/controller/ControllerTest.java
+++ test/controller/ControllerTest.java
@@ -1,11 +1,29 @@
 package controller;
 
+import ordination.Laegemiddel;
+import ordination.Patient;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
 
     @org.junit.jupiter.api.Test
     void opretDagligFastOrdination() {
+        //arrange
+        Laegemiddel l1 = new Laegemiddel("Paracetamol", 1,
+                1.5, 2, "MI");
+        Patient p1 = new Patient("121256-0512", "Jane Jensen", 24.0);
+        //act
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            Controller.getController().opretDagligFastOrdination(LocalDate.of(2024, 8, 25), LocalDate.of(2024, 8, 21), p1, l1, 1, 2, 1, 1);
+        });
+        int i = 3;
+        //fastOrdination = Controller.getController().opretDagligFastOrdination(LocalDate.of(2024, 8, 14), LocalDate.of(2024, 8, 21), p1, l1, 1, 2, 1, 1);
+        //assert
+        assertEquals(exception.getMessage(), "Startdato er efter slutdato");
+        assertEquals(p1.getOrdinationer().contains());
     }
 
     @org.junit.jupiter.api.Test
