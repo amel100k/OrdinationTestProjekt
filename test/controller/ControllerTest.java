a4c56a44777b062798748c92307d7455b00018cf
lucas <minibba@protonmail.com>
2024-03-20 08:45:07 +0100

    test

---
+++ test/controller/ControllerTest.java
@@ -0,0 +1,26 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void opretDagligFastOrdination() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void opretDagligSkaevOrdination() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ordinationPNAnvendt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void anbefaletDosisPrDoegn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void antalOrdinationerPrVægtPrLægemiddel() {
+    }
+}
