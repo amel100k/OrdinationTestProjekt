c09dfb71183a0bf78de485c344d4d44c7076e803
lucas <minibba@protonmail.com>
2024-03-20 10:02:00 +0100

    antal og anvendt

--- test/controller/ControllerTest.java
+++ test/controller/ControllerTest.java
@@ -1,4 +1,12 @@
 package controller;
+import ordination.Laegemiddel;
+import ordination.Ordination;
+import ordination.PN;
+import ordination.Patient;
+import org.junit.jupiter.api.DisplayName;
+import storage.Storage;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,13 +22,26 @@ class ControllerTest {
 
     @org.junit.jupiter.api.Test
     void ordinationPNAnvendt() {
+        Controller.getController().opretLaegemiddel("Fucidin", 1,1.5,2,"MI");
+        Controller.getController().opretPatient("121256-0512","Jane Jensen",63.4);
+        PN pn = Controller.getController().opretPNOrdination(LocalDate.of(2021,1,1),LocalDate.of(2021,1,12),Controller.getController().getAllPatienter().getFirst(),Controller.getController().getAllLaegemidler().getFirst(),123);
+        assertTrue(pn.givDosis(LocalDate.of(2021,1,2)));
+        assertThrows(IllegalArgumentException.class, () -> Controller.getController().ordinationPNAnvendt(pn, LocalDate.of(2021,1,13)));
+        assertThrows(IllegalArgumentException.class,() -> Controller.getController().ordinationPNAnvendt(pn,LocalDate.of(2020,12,12)));
     }
-
     @org.junit.jupiter.api.Test
     void anbefaletDosisPrDoegn() {
     }
 
     @org.junit.jupiter.api.Test
+    @DisplayName("AntalOrdinationerPrVægtPrLægemiddel")
     void antalOrdinationerPrVægtPrLægemiddel() {
+        Controller.getController().opretLaegemiddel("Fucidin",1,1.5,2,"MI");
+        Controller.getController().opretPatient("121256-0512","Jane Jensen",63.4);
+        Controller.getController().opretPNOrdination(LocalDate.of(2021,1,1),LocalDate.of(2021,1,12),Controller.getController().getAllPatienter().getFirst(),Controller.getController().getAllLaegemidler().getFirst(),123);
+        Controller.getController().opretPatient("121256-0512","Lene Jensen",163.4);
+        Controller.getController().opretPNOrdination(LocalDate.of(2021,1,1),LocalDate.of(2021,1,12),Controller.getController().getAllPatienter().get(1),Controller.getController().getAllLaegemidler().getFirst(),123);
+        assertEquals(1,Controller.getController().antalOrdinationerPrVægtPrLægemiddel(50,100,Controller.getController().getAllLaegemidler().getFirst()));
+        assertEquals(0,Controller.getController().antalOrdinationerPrVægtPrLægemiddel(120,100, Controller.getController().getAllLaegemidler().getFirst()));
     }
 }
